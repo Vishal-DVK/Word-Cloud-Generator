bd6a98754961692644f6a6e8cd169f26ed68f702
Dosapati Vishal <142665847+Vishal-DVK@users.noreply.github.com>
2025-05-31 11:16:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def generate_wordcloud():
         return f"Server error: {e}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
